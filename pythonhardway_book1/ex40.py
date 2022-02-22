c9532b60d605db16daaac85b32b9f4b391d4d33b
RDMills <rdmills@aurorak12.org>
2022-02-22 12:52:50 -0700

    Enter

--- pythonhardway_book1/ex40.py
+++ pythonhardway_book1/ex40.py
@@ -1,1 +1,28 @@
+#Modules
+mystuff = {'apple': "I am Aples!"}
+print(mystuff['apple'])
 
+import mystuff
+mystuff.apple()
+
+def apple():
+    print("I am Apples")
+
+#This is just a variable
+tangerine = "Living refelction of a dream!"
+
+import mystuff
+mystuff.apple()
+print(mystuff.tangerine)
+
+mystuff['apple'] #get apple from the dictionary
+mystuff.apple() # get apple from the module
+mystuff.tangerine #same thing, it's just a variable
+
+class MyStuff(object):
+
+    def _init_(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I am apples!")
---
+++ pythonhardway_book1/mystuff.py
@@ -0,0 +1,6 @@
+# this goes in mystuff.py
+def apple():
+    print("I am Apples!")
+
+#This is just a variable
+tangerine = "Living refelction of a dream!"
