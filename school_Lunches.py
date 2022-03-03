4b2b6154e7b344eb1c567f22f004b632274ff7cc
RDMills <rdmills@aurorak12.org>
2022-03-03 13:44:45 -0700

    New practice file

---
+++ school_Lunches.py
@@ -0,0 +1,14 @@
+#Create a program to ask the students about their lunch habits
+from tkinter.messagebox import YES
+
+
+eat_lunch_out = input("Do you eat lunch out during school? yes or no  ")
+if (eat_lunch_out  =="yes"):
+    eat_lunch_out_choice = True
+    eat_lunch_out = input("Do you eat at Wendys, McDonalds, Burger King, Subway, Little Caesars, King Soopers, none   " )
+    print ("You said: " + eat_lunch_out)
+else: 
+    eat_lunch_out_choice = False
+    print("You must eat at School")
+
+
