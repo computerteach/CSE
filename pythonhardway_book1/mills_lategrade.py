7df1938928e88d7020f81c00754e7ec85a88a617
RDMills <rdmills@aurorak12.org>
2022-04-13 08:05:50 -0600

    remove

--- pythonhardway_book1/mills_lategrade.py
+++
@@ -1,7 +0,0 @@
-totalearned = int(input("What was your grade? (out of 100): "))
-late = input("Was your assignment turned late? (true or false):  ")
-if late == 'false':
-    print("Your assignment was turned in on time, your grade is {totalearned}/100".format(totalearned=totalearned))
-else:
-    totalearned = totalearned/2
-    print("Your assignment was not turned in on time, your grade is {totalearned}/100".format(totalearned=totalearned))
