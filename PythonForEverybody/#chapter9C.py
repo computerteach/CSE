03bf527f8ccf8dd0f133a3f147156b06e833ed3e
RDMills <rdmills@aurorak12.org>
2022-04-14 14:43:04 -0600

    latest work

--- PythonForEverybody/#chapter9C.py
+++ PythonForEverybody/#chapter9C.py
@@ -6,8 +6,8 @@ handle = open(name)
 
 counts = dict()
 for line in handle:
-    words = line.split()
-    for word in words:
+   words = line.split()
+   for word in words:
         counts[word] = counts.get(word, 0) + 1
 
 bigcount = None
